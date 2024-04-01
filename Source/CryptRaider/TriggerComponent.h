32a19bb33cb1688ad8600a296085f21115d3fc34
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2024-04-01 14:58:00 +0900

    Trigger Component 생성

---
+++ Source/CryptRaider/TriggerComponent.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/BoxComponent.h"
+#include "TriggerComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class CRYPTRAIDER_API UTriggerComponent : public UBoxComponent
+{
+	GENERATED_BODY()
+public:
+	UTriggerComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	
+};
